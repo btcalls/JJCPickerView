109544865831c5fed6608c96d7d9b7c6c3911189
Jason Jon E. Carreos <jason@ingenuity.ph>
2019-06-07 21:43:44 +0800

    Project setup

---
+++ JJCPickerView/JJCPickerView.h
@@ -0,0 +1,19 @@
+//
+//  JJCPickerView.h
+//  JJCPickerView
+//
+//  Created by Jason Jon E. Carreos on 07/06/2019.
+//  Copyright © 2019 Jason Jon E. Carreos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJCPickerView.
+FOUNDATION_EXPORT double JJCPickerViewVersionNumber;
+
+//! Project version string for JJCPickerView.
+FOUNDATION_EXPORT const unsigned char JJCPickerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJCPickerView/PublicHeader.h>
+
+
